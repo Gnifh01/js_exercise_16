37e22fd1c612684af31c29c059d5be27e8a17ce5
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-05-20 11:53:55 +0200

    first commit

---
+++ exercise-3.js
@@ -0,0 +1,24 @@
+function createStore() {
+  let store = [];
+  return function addStore(product) {
+    for (const key in product) {
+      if (Object.entries(key)) {
+        console.log(product.name);
+      }
+      return store;
+    }
+  };
+}
+
+const redPants = { id: 1, name: "Red Pants" };
+const whiteHat = { id: 2, name: "White Hat" };
+const blackSneakers = { id: 3, name: "Black Sneakers" };
+
+console.log("--- Dress Store ---");
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat);
+
+console.log("--- Shoes Store ---");
+const shoesStore = createStore();
+shoesStore(blackSneakers);
